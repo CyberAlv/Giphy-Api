2f757e7b3f7140ac6d8f69671eea7c9c9de87905
CyberAlv <munaralvin@gmail.com>
2020-06-04 18:33:29 -0400

    added api on code, it runs, but wont show giphy

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SearchBar extends Component {
-    constructor() {
-        super();
-        this.state = { term: '' }
+    constructor(props) {
+        super(props);
+        this.state = { term:"",
+        state: "Regular",
+        gifs: this.props.gifs,
+        order: "default"
+        }
     }
 
     onInputChange(term) {
         this.setState({term});
         this.props.onSearchChange(term);
     }
-
-    search() {
-        this.props.onSearch(this.state.term);
-        console.log('results: ')
+    
+    componentDidMount(){
+        axios.get.apply('http://api.giphy.com/v1/gifs/search?q=%27' + this.state.term).then((response) => {
+            this.setState( {gifs: response.data ["data"], state: "searching"});
+            this.state.gids.forEach((element) => {console.log(element.ratings)})
+        })
     }
+    
+    search = (result) => {
+        this.props.onSearch(this.state.term); {
+            axios.get.apply('http://api.giphy.com/v1/gifs/random?api_key=0aIzVnzB2PPXO4Mtz0ZQNPALfV7CxgIT').then((response) => {
+                this.setState({gifs: response.data ["data"], state: "searching"})
+            });
+        }             
+        console.log('results: ')
+        }
 
     pressEnter = (event) => {
         if(event.key === 'Enter'){
@@ -27,10 +43,11 @@ class SearchBar extends Component {
         return (
             <div className="search" type="submit">
                 <input onTermChange={event => this.onInputChange(event.target.value)} onKeyPress={this.pressEnter} />
-                <a onClick={this.search}>Giph Me</a>
-            </div>
+                <a onClick={this.search}>Giph Me</a>              
+            </div>          
         );
     }
 }
+
 
 export default SearchBar;
